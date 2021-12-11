9a5adadbf8fcde003bc40febaf9a35da4758e36e
syminical <sym@symys.org>
2021-12-11 14:17:54 -0500

    added polls url namespace (app_name)

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.template import loader
 
@@ -11,7 +11,8 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = f'You\'re looking at the results of question {question_id}.'
