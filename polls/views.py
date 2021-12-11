3ff5f19d7a7c9c62a6bd98bd4944b3b92bf846ea
syminical <sym@symys.org>
2021-12-11 01:44:13 -0500

    created detail, results, vote views for Question

--- polls/views.py
+++ polls/views.py
@@ -5,3 +5,13 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = f'You\'re looking at the results of question {question_id}.'
+    return HttpResponse(response)
+
+def vote(request, question_id):
+    return HttpResponse(f'You\'re voting on question {question_id}.')
