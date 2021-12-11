d0d84f31473c982ade9be6ce83d5ceeea15a5c78
syminical <sym@symys.org>
2021-12-11 14:46:18 -0500

    modified vote view. returns response to POST
    checks if a choice was selected and that it exists in db. rerenders question  with error or increments vote and redirects to result.

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from django.urls import reverse
 
-from .models import Question
+from .models import Question, Choice
 
 # Create your views here.
 def index(request):
@@ -19,4 +20,15 @@ def results(request, question_id):
     return HttpResponse(response)
 
 def vote(request, question_id):
-    return HttpResponse(f'You\'re voting on question {question_id}.')
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
