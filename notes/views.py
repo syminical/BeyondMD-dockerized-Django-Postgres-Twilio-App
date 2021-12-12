c8759bb0effd2f48ca018f13509ca66aad99d110
syminical <sym@symys.org>
2021-12-12 12:49:31 -0500

    enabled notes index

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views import generic
+
+from .models import Task, TaskList
 
 # Create your views here.
+class IndexView(generic.ListView):
+    template_name = 'notes/index.html'
+    context_object_name = 'task_lists'
+
+    def get_queryset(self):
+        """
+        Return all lists available.
+        """
+        return TaskList.objects.all()
